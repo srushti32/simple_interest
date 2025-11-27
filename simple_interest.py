718cbd4084af02814a4f434648ad8b51f81dec93
srushti32 <hsrushti320@gmail.com>
2025-11-27 19:24:57 +0530

    Create simple_interest.py

---
+++ simple_interest.py
@@ -0,0 +1,12 @@
+import sys
+if len(sys.argv)! =4:
+print("usage:python simple_interest.py <principal> <rate> <time>")
+sys.exit(1)
+
+p = float(sys.argv[1])
+R = float(sys.argv[2])
+T = float(sys.argv[3])
+
+s1 = (P * R * T) / 100
+
+print("simple interest:", s1)
