ceffe0a9afbed9e2ae53a61bb841a8d48653e408
srushti32 <hsrushti320@gmail.com>
2025-11-28 15:34:29 +0530

    Update simple_interest.py

--- simple_interest.py
+++ simple_interest.py
@@ -1,12 +1,15 @@
 import sys
-if len(sys.argv)! =4:
-print("usage:python simple_interest.py <principal> <rate> <time>")
-sys.exit(1)
+
+if len(sys.argv) != 4:
+    print("usage: python simple_interest.py <principal> <rate> <time>")
+    sys.exit(1)
 
 p = float(sys.argv[1])
 R = float(sys.argv[2])
 T = float(sys.argv[3])
 
-s1 = (P * R * T) / 100
+s1 = (p * R * T) / 100
 
 print("simple interest:", s1)
+
+
